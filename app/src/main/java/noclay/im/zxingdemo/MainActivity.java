11a2458c174fbbb4a9726e21752aeccb05b41d4b
NoClay <826611389@qq.com>
2017-08-08 15:15:06 +0800

    提交说明

--- app/src/main/java/noclay/im/zxingdemo/MainActivity.java
+++ app/src/main/java/noclay/im/zxingdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package noclay.im.zxingdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import noclay.im.zxing.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivityForResult(new Intent(this, CaptureActivity.class), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) { //RESULT_OK = -1
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            Toast.makeText(MainActivity.this, scanResult, Toast.LENGTH_LONG).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
